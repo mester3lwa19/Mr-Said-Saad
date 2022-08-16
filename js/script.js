272ac76279491ce23c0a128798ad2a5dcbc2833d
mester3lwa19 <tomaskhaled196@gmail.com>
2022-08-16 19:23:32 +0200

    V2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 let preloader = document.querySelector(".preloader");
+let toggle = document.querySelector(".toggle");
+let display = document.querySelector(".display");
+let sideBar = document.querySelector(".side-bar");
 window.addEventListener("load",function() {
     preloader.style.display = "none"
+})
+toggle.addEventListener("click", function () {
+    sideBar.classList.toggle("display")
 })
